ff42278fe244fcdf00891368e7c6295ea25b438f
JonhLucas <jlas@ic.ufal.br>
2022-01-11 18:23:38 -0300

    Navigation buttons

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -22,7 +22,6 @@ function Listing(){
                     <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
                         <MovieCard />
                     </div>
-                    
                 </div> 
             </div>
             
